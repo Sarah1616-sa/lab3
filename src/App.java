7416276f70f2dd9cefca3231033cfceb7d60c9f0
s202278520 <s202278520@kfupm.edu.sa>
2024-09-23 13:58:46 +0300

    from forkan

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Hello, heloo");
-        
+
+        System.out.println("from forkan to sara222");
     }
 }
